c6d075ad68aeb280f91e8e554d6a4d34e4790428
Veeresh Kumar <veerubhai5c1@users.noreply.github.com>
2014-10-01 12:10:30 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 sudo docker run -P -name rs1_srv1 -d veeresh/mongodb --replSet rs1 --noprealloc --smallfiles
 sudo docker run -P -name rs1_srv2 -d veeresh/mongodb --replSet rs1 --noprealloc --smallfiles
 sudo docker run -P -name rs1_srv3 -d veeresh/mongodb --replSet rs1 --noprealloc --smallfiles
