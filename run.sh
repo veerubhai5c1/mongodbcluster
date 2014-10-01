189060b60294719bcba42418fed8e3445de60038
Veeresh Kumar <veerubhai5c1@users.noreply.github.com>
2014-10-01 12:14:58 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+echo "Starting mongodb shard server1 for replica set1..."
 sudo docker run -P -name rs1_srv1 -d veeresh/mongodb --replSet rs1 --noprealloc --smallfiles
+echo "Starting mongodb shard server2 for replica set1..."
 sudo docker run -P -name rs1_srv2 -d veeresh/mongodb --replSet rs1 --noprealloc --smallfiles
+echo "Starting mongodb shard server3 for replica set1..."
 sudo docker run -P -name rs1_srv3 -d veeresh/mongodb --replSet rs1 --noprealloc --smallfiles
 
+echo "Starting mongodb shard server1 for replica set2..."
 sudo docker run -P -name rs2_srv1 -d veeresh/mongodb --replSet rs2 --noprealloc --smallfiles
+echo "Starting mongodb shard server2 for replica set2..."
 sudo docker run -P -name rs2_srv2 -d veeresh/mongodb --replSet rs2 --noprealloc --smallfiles
+echo "Starting mongodb shard server3 for replica set2..."
 sudo docker run -P -name rs2_srv3 -d veeresh/mongodb --replSet rs2 --noprealloc --smallfiles
+
